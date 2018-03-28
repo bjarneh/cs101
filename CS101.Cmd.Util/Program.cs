0879c2480dfb7f0a2fce837483ecfaddb911448b
bjarne holen <bjarneholen@gmail.com>
2018-03-28 19:06:06 +0200

    Added conversion lib

--- CS101.Cmd.Util/Program.cs
+++ CS101.Cmd.Util/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using CS101.IO.Util;
 
 /**
  * Hello World example to test out C#
@@ -11,32 +15,81 @@ namespace CS101.Cmd.Util
 {
     class Program
     {
+
+        static string help = @"
+        conv - convert file encoding
+
+        usage: conv [OPTIONS] <FILENAME(S)>
+
+        options:
+        -h --help   : print this menu and exit
+        -f --from   : input encoding (UTF-8,ISO-8859-1,...)
+        -t --to     : output encoding (UTF-8,UTF-16,...)
+        ";
+
         static void Main(string[] args)
         {
+            string inputFile = null;
+            string inputStrEnc = null;
+            string outputStrEnc = null;
+            Encoding inputEncoding = null;
+            Encoding outputEncoding = null;
+
             var getopt = new GetOpt();
 
             getopt.AddBoolOption(flags: new string[]{"-h","-help","--help"});
             getopt.AddStrOption(flags:
-                 new string[]{"-f","-f=","-file","-file=","--file","--file="});
+                 new string[]{"-f","-f=","-from","-from=","--from","--from="});
+            getopt.AddStrOption(flags:
+                 new string[]{"-t","-t=","-to","-to=","--to","--to="});
 
             string[] rest = getopt.Parse(argv: args);
 
             if( getopt.IsSet("--help") ){
-                Console.WriteLine("I'll help you!");
+                var helpFmt = String.Join("\n",
+                        help.Split("\n")
+                            .Select(s => String.Format("  {0}", s.Trim()) ));
+                Console.WriteLine( helpFmt );
+                Environment.Exit(0);
             }
 
-            if( getopt.IsSet("-file") ){
-                string opt = getopt.GetOption("-file");
-                Console.WriteLine($"File name given: {opt}");
+            if( rest.Length == 0 ){
+                Console.Error.WriteLine("No filename given");
+                Environment.Exit(1);
+            }else{
+                inputFile = rest[0];
             }
 
+            if( getopt.IsSet("-from")){
+                inputStrEnc = getopt.GetOption("-from");
+            }
+            if( getopt.IsSet("-to")){
+                outputStrEnc = getopt.GetOption("-to");
+            }
+
+            if( inputStrEnc == null ){
+                Console.Error.WriteLine("No input encoding given");
+                Environment.Exit(1);
+            }
+
+            if( outputStrEnc == null ){
+                Console.Error.WriteLine("No output encoding given");
+                Environment.Exit(1);
+            }
+
+            outputEncoding = Encoding.GetEncoding( outputStrEnc );
+            inputEncoding  = Encoding.GetEncoding( inputStrEnc );
+
+            Conv.Convert(inputFile, inputEncoding, outputEncoding);
+
+            /*
             if( rest.Length > 0 ){
                 foreach(string r in rest){
                     Console.WriteLine($"rest: {r}");
                 }
             }
-
-            Console.WriteLine( $" getopt: {getopt}" );
+            */
+            //Console.WriteLine( $" getopt: {getopt}" );
         }
     }
 }
---
+++ CS101.IO.Util/Conv.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+/**
+ * Read files in one encoding and convert them into another encoding.
+ * 
+ * @author  bjarneh@ifi.uio.no
+ * @license  public domain
+ */
+
+namespace CS101.IO.Util
+{
+    public static class Conv
+    {
+        public static void Convert(string inputFile, string inputEnc, string outputEnc){
+            Convert(inputFile,
+                    Encoding.GetEncoding(inputEnc),
+                    Encoding.GetEncoding(outputEnc));
+        }
+
+
+        public static void Convert(string fileName, Encoding inputEnc, Encoding outputEnc){
+            Stream inputStream = getInputAsStream(fileName, inputEnc);
+        }
+
+
+        private static Stream getInputAsStream(string inputFile, Encoding inputEnc){
+            using( MemoryStream binaryWriter = new MemoryStream() ){
+                using( BinaryReader binaryReader = new BinaryReader(File.Open(inputFile, FileMode.Open), inputEnc) ){
+                    binaryReader.BaseStream.CopyTo( binaryWriter );
+                }
+                return binaryWriter;
+            }
+        }
+
+        private static void writeStreamInEncoding(string fileName, Stream stream, Encoding outputEncoding){
+            using( BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileName, FileMode.Open), outputEncoding)){
+                binaryWriter.BaseStream.CopyTo( stream );
+            }
+        }
+
+    }
+}
